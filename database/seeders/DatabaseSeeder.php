5cd45a07912fc4cce397ddbc0ca38c12423bc679
BlankaCorp <nelghammaz@gmail.com>
2023-03-28 12:17:17 +0200

    Role[Build] : Model + Seeders + Migration

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,6 +17,7 @@ class DatabaseSeeder extends Seeder
         $this->call([
             ArtistSeeder::class,
             TypeSeeder::class,
+            RoleSeeder::class,
         ]);
     }
 
---
+++ database/seeders/RoleSeeder.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB; 
+use App\Models\Role;
+
+class RoleSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //Empty the table first
+        Role::truncate();
+        //Define data
+        $roles = [
+            ['role'=>'comédien'],
+            ['role'=>'scénographe'],
+            ['role'=>'auteur'],
+        ];
+        //Insert data in the table
+        DB::table('roles')->insert($roles);
+    }
+}
