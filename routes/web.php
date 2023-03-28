1ee38985159c060f7ce729f0791cf4effb59b5d9
BlankaCorp <nelghammaz@gmail.com>
2023-03-28 10:54:00 +0200

    Artist[Build] : Controller + Template + Routes

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,8 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ArtistController;
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -17,6 +19,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/artist', [ArtistController::class, 'index'])->name('artist_index');
+Route::get('/artist/{id}', [ArtistController::class, 'show'])
+	->where('id', '[0-9]+')->name('artist_show');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
