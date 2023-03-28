af8b1f7099890e810da12954d883776d60751de9
BlankaCorp <nelghammaz@gmail.com>
2023-03-28 11:17:50 +0200

    Artist[Feature] : Edit (Name+Firstname)

--- app/Http/Controllers/ArtistController.php
+++ app/Http/Controllers/ArtistController.php
@@ -67,7 +67,11 @@ class ArtistController extends Controller
      */
     public function edit($id)
     {
-        //
+        $artist = Artist::find($id);
+        
+        return view('artist.edit',[
+            'artist' => $artist,
+        ]);
     }
 
     /**
@@ -79,8 +83,23 @@ class ArtistController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+	   //Validation des données du formulaire
+        $validated = $request->validate([
+            'firstname' => 'required|max:60',
+            'lastname' => 'required|max:60',
+        ]);
+
+	   //Le formulaire a été validé, nous récupérons l’artiste à modifier
+        $artist = Artist::find($id);
+
+	   //Mise à jour des données modifiées et sauvegarde dans la base de données
+        $artist->update($validated);
+
+        return view('artist.show',[
+            'artist' => $artist,
+        ]);
     }
+
 
     /**
      * Remove the specified resource from storage.
