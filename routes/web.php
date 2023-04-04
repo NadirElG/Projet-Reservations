894516547008ef135da94bc7f250963fe9a66c28
BlankaCorp <nelghammaz@gmail.com>
2023-04-04 16:56:57 +0200

    Locality[Build] : Controller + Templates + Routes~

--- routes/web.php
+++ routes/web.php
@@ -6,6 +6,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ArtistController;
 use App\Http\Controllers\TypeController;
 use App\Http\Controllers\RoleController;
+use App\Http\Controllers\LocalityController;
 
 
 /*
@@ -43,7 +44,10 @@ Route::get('/type/{id}', [TypeController::class, 'show'])
 Route::get('/role', [RoleController::class, 'index'])->name('role.index');
 Route::get('/role/{id}', [RoleController::class, 'show'])
         ->where('id', '[0-9]+')->name('role.show');
-    
+
+Route::get('/locality', [LocalityController::class, 'index'])->name('locality.index');
+Route::get('/locality/{id}', [LocalityController::class, 'show'])
+        ->where('id', '[0-9]+')->name('locality.show');
 
 
 
