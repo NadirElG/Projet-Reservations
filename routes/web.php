e2f78a4cc906f349e5fd92baa7e6b7d0466d0dd7
BlankaCorp <nelghammaz@gmail.com>
2023-04-04 16:39:56 +0200

    Role[Build] : Controller + Templates + Routes

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ArtistController;
 use App\Http\Controllers\TypeController;
+use App\Http\Controllers\RoleController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -37,6 +39,10 @@ Route::delete('/artist/{id}', [ArtistController::class, 'destroy'])
 Route::get('/type', [TypeController::class, 'index'])->name('type.index');
 Route::get('/type/{id}', [TypeController::class, 'show'])
             ->where('id', '[0-9]+')->name('type.show');
+
+Route::get('/role', [RoleController::class, 'index'])->name('role.index');
+Route::get('/role/{id}', [RoleController::class, 'show'])
+        ->where('id', '[0-9]+')->name('role.show');
     
 
 
