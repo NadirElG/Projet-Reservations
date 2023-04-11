6e0272d22df9054eab7377ed19be3cd6bb8dbd56
BlankaCorp <nelghammaz@gmail.com>
2023-04-11 10:49:57 +0200

    Test : debug(dashboard.blade.php)

--- README.txt
+++ README.txt
@@ -2,3 +2,4 @@
     -> Ne fonctionne pas. 
     -> Modification à régler.
 
+- Pg 54 : Répètez les opérations pour les relations entre les modèles Users et Representation
--- resources/views/artist/index.blade.php
+++ resources/views/artist/index.blade.php
@@ -3,28 +3,42 @@
 @section('title', 'Liste des artistes')
 
 @section('content')
-    <h1>Liste des {{ $resource }}</h1>
+    <div class="container">
+        <h1 class="mb-4">Liste des {{ $resource }}</h1>
 
-    <ul>
-        <li><a href="{{ route('artist.create') }}">Ajouter</a></li>    
-    </ul>
+        <div class="d-flex justify-content-between align-items-center mb-4">
+            <ul class="list-unstyled mb-0">
+                <li><a href="{{ route('artist.create') }}" class="btn btn-primary">Ajouter un artiste</a></li>
+            </ul>
 
-    <table>
-        <thead>
-            <tr>
-                <th>Firstname</th>
-                <th>Lastname</th>
-            </tr>
-        </thead>
-        <tbody>
-        @foreach($artists as $artist)
-            <tr>
-                <td>{{ $artist->firstname }}</td>
-                <td>
-                    <a href="{{ route('artist.show', $artist->id) }}">{{ $artist->lastname }}</a>
-                </td>
-            </tr>
-        @endforeach
-        </tbody>
-    </table>
+            <form action="{{ route('logout') }}" method="POST">
+                @csrf
+                <button type="submit" class="btn btn-outline-danger">Déconnexion</button>
+            </form>
+        </div>
+
+        <div class="table-responsive">
+            <table class="table table-hover">
+                <thead>
+                    <tr>
+                        <th scope="col">Nom</th>
+                        <th scope="col">Prénom</th>
+                        <th scope="col"></th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($artists as $artist)
+                        <tr>
+                            <td>{{ $artist->lastname }}</td>
+                            <td>{{ $artist->firstname }}</td>
+                            <td>
+                                <a href="{{ route('artist.show', $artist->id) }}" class="btn btn-sm btn-outline-primary">Voir</a>
+                                <a href="{{ route('artist.edit', $artist->id) }}" class="btn btn-sm btn-outline-secondary">Modifier</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </div>
+    </div>
 @endsection
