416f23255053196519582080481178678db6f1c9
BlankaCorp <nelghammaz@gmail.com>
2023-05-25 18:14:58 +0200

    User[Model] : navbar(name + role)

--- app/Models/User.php
+++ app/Models/User.php
@@ -42,22 +42,35 @@ class User extends Authenticatable
         'email_verified_at' => 'datetime',
     ];
 
-    public function representations() {
+    public function representations()
+    {
         return $this->belongsToMany(Representation::class, 'reservations')->using(Reservation::class)->withTimestamps();
     }
 
-    public function role() {
-        return $this->belongsTo(Role::class);
+    public function role()
+    {
+    return $this->belongsTo(Role::class, 'role_id');
     }
+
+    public function roles()
+    {
+    return $this->belongsToMany(Role::class, 'user_roles')->withTimestamps();
+    }
+
+    public function assignDefaultRole()
+    {
+    $defaultRoleId = 2; // ID du rôle "member"
+    $this->roles()->attach($defaultRoleId);
+    }
+
 
     protected static function boot()
     {
-        parent::boot();
+    parent::boot();
 
-        static::created(function ($user) {
-            // Attribuer le rôle "member" à l'utilisateur
-            $user->role()->associate(2); // 2 correspond à l'ID du rôle "member"
-            $user->save();
-        });
+    static::created(function ($user) {
+        $user->assignDefaultRole();
+    });
     }
+
 }
--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -1,51 +1,39 @@
-<script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.7.0/dist/js/bootstrap.bundle.min.js"></script>
-
-
 <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
     <div class="container">
         <a class="navbar-brand" href="{{ url('/') }}">
             {{ config('app.name', 'Laravel') }}
         </a>
 
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">
-            <!-- Left Side Of Navbar -->
-            <ul class="navbar-nav mr-auto">
-
-            </ul>
-
-            <!-- Right Side Of Navbar -->
-            <ul class="navbar-nav ml-auto">
-                <!-- Authentication Links -->
-                @guest
+        <!-- Right Side Of Navbar -->
+        <ul class="navbar-nav ml-auto">
+            <!-- Authentication Links -->
+            @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                </li>
+                @if (Route::has('register'))
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
+                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
                     </li>
-                    @if (Route::has('register'))
-                        <li class="nav-item">
-                            <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                        </li>
-                    @endif
-                @else
-                    <li class="nav-item dropdown">
-                        <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                            {{ Auth::user()->name }} <span class="caret"></span>
-                        </a>
+                @endif
+            @else
+                <li class="nav-item ml-auto">
+                    <span class="navbar-text">
+                        {{ Auth::user()->name }} ({{ Auth::user()->roles->first()->role }})
+                    </span>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('logout') }}"
+                        onclick="event.preventDefault();
+                                         document.getElementById('logout-form').submit();">
+                        {{ __('Logout') }}
+                    </a>
 
-                        <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                            <a class="dropdown-item" href="{{ route('logout') }}"
-                                onclick="event.preventDefault();
-                                                 document.getElementById('logout-form').submit();">
-                                {{ __('Logout') }}
-                            </a>
-
-                            <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                                @csrf
-                            </form>
-                        </div>
-                    </li>
-                @endguest
-            </ul>
-        </div>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
+                </li>
+            @endguest
+        </ul>
     </div>
 </nav>
