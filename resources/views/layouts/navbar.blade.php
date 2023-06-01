24c5de047ae1a8b22de964457891f12de40cb3f1
BlankaCorp <nelghammaz@gmail.com>
2023-06-01 18:31:34 +0200

    Navbar[update] : theatres

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -4,19 +4,48 @@
             {{ config('app.name', 'Laravel') }}
         </a>
 
-        <!-- Right Side Of Navbar -->
-        <ul class="navbar-nav ml-auto">
-            <!-- Authentication Links -->
-            @guest
+        <!-- Toggled button for mobile navigation -->
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <!-- Navbar links -->
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('artist.index') }}">Artistes</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('role.index') }}">Role</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('show_index') }}">Spectacles</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('theatres') }}">API Spectacles</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('location.index') }}">Location</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('locality.index') }}">Localité</a>
+                </li>
+            </ul>
+
+            <!-- Right Side Of Navbar -->
+            <ul class="navbar-nav ml-auto">
+                <!-- Authentication Links -->
+                @guest
                 <li class="nav-item">
                     <a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
                 </li>
                 @if (Route::has('register'))
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
-                    </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a>
+                </li>
                 @endif
-            @else
+                @else
                 <li class="nav-item ml-auto">
                     <span class="navbar-text">
                         {{ Auth::user()->name }} ({{ Auth::user()->roles->first()->role }})
@@ -26,17 +55,16 @@
                 <a class="nav-link" href="{{ route('dashboard') }}">Dashboard</a>
                 @endif
                 <li class="nav-item">
-                    <a class="nav-link" href="{{ route('logout') }}"
-                        onclick="event.preventDefault();
-                                         document.getElementById('logout-form').submit();">
+                    <a class="nav-link" href="{{ route('logout') }}" onclick="event.preventDefault();
+                                             document.getElementById('logout-form').submit();">
                         {{ __('Logout') }}
                     </a>
-
                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                         @csrf
                     </form>
                 </li>
-            @endguest
-        </ul>
+                @endguest
+            </ul>
+        </div>
     </div>
 </nav>
--- resources/views/theatres.blade.php
+++ resources/views/theatres.blade.php
@@ -1,38 +1,24 @@
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Liste des événements Ticketmaster</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-    <style>
-        .pagination li .page-link {
-            display: none;
-        }
-        
-        .pagination li:first-child .page-link,
-        .pagination li:last-child .page-link {
-            display: inline-block;
-        }
-        </style>
-        
-</head>
-<body>
-    <div class="container">
-        <div class="row">
-            @foreach ($theatres as $theatre)
-                <div class="col-12 col-md-4">
-                    <div class="card mb-4 shadow-sm">
-                        <img class="card-img-top" src="{{ $theatre['images'][0]['url'] }}" alt="{{ $theatre['name'] }}">
-                        <div class="card-body">
-                            <h5 class="card-title">{{ $theatre['name'] }}</h5>
-                            <p class="card-text">{{ $theatre['dates']['start']['localDate'] }}</p>
-                            <p class="card-text">{{ $theatre['_embedded']['venues'][0]['name'] }}</p>
-                            <a href="{{ $theatre['url'] }}" class="btn btn-primary">Buy Tickets</a>
-                        </div>
+
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        @foreach ($theatres as $theatre)
+            <div class="col-12 col-md-4">
+                <div class="card mb-4 shadow-sm">
+                    <img class="card-img-top" src="{{ $theatre['images'][0]['url'] }}" alt="{{ $theatre['name'] }}">
+                    <div class="card-body">
+                        <h5 class="card-title">{{ $theatre['name'] }}</h5>
+                        <p class="card-text">{{ $theatre['dates']['start']['localDate'] }}</p>
+                        <p class="card-text">{{ $theatre['_embedded']['venues'][0]['name'] }}</p>
+                        <a href="{{ $theatre['url'] }}" class="btn btn-primary">Buy Tickets</a>
                     </div>
                 </div>
-            @endforeach
-        </div>
-        
+            </div>
+        @endforeach
     </div>
-</body>
-</html>
+    
+</div>
+
+@endsection
